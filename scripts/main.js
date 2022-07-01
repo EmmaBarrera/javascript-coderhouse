346c164e42c1e3bd8dd6b4a8a8b6e70862aeaf33
Emmanuel Barrera <emmanuelbarrera.coach@gmail.com>
2022-06-30 23:56:12 -0300

    Fetch

--- scripts/main.js
+++ scripts/main.js
@@ -1,71 +1,67 @@
-//* 1. CREO UNA CLASE CONSTRUCTORA PARA LOS PRODUCTOS:
+//* 1. CREO UNA VARIABLE A LA QUE LUEGO LE ASIGNARE LOS PRODUCTOS:
 
-class Wallets {
-    constructor(name, price, description, img, id) {
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.img = img;
-        this.id = id;
-    }
-};
+let products
 
 
-//* 2. CREO LOS PRODUCTOS:
-
-const product1 = new Wallets('NANO X', '$17.080,00', 'Protege, compra, cambia, haz crecer tus cripto y gestiona tus NFTs con nuestra nueva billetera de hardware con Bluetooth. Todos tus activos digitales protegidos en un mismo lugar.', 'https://cdn.shopify.com/s/files/1/2974/4858/products/01_4f10d168-302a-47b3-b79f-e0a6b6da7a48.png?v=1644216184', '040041');
-
-const product2 = new Wallets('NANO S PLUS', '$9.569,00', 'Protege tus activos y explora la Web3 con el nuevo Ledger Nano S Plus. Tomar el emblemático Ledger Nano S y transformarlo en amigable para las DeFi y los NFTs. Eso es lo que hicimos con el nuevo Nano S Plus.', 'https://cdn.shopify.com/s/files/1/2974/4858/products/01_6.png?v=1647271638', '040042');
-
-const product3 = new Wallets('NANO S', '$6.595,00', 'Protege, compra y haz crecer tus activos cripto con la billetera de hardware más famosa del mundo.', 'https://cdn.shopify.com/s/files/1/2974/4858/products/black-lns_95b4bf48-c245-4dc5-8db7-7b437ce5bdc5.png?v=1644216128', '040043');
-
-
-//* 3. CREO UN ARRAY CON LOS PRODUCTOS:
-
-let products = [product1, product2, product3];
-
-
-//* 4. CREO EL CARRITO VACIO:
+//* 2. CREO UNA VARIALBE CON ARRAY PARA EL CARRITO VACIO:
 
 let cart = [];
 
-//* 5. ACCEDO AL GRUPO DE LOS PRODUCTOS EN MI HTML Y LO ASIGNO COMO VALOR A LA VARIABLE PRODUCTSCONTAINER:
 
-const productsContainer = document.querySelector('.card-group');
+//* 3. ACCEDO AL GRUPO DE LOS PRODUCTOS EN MI HTML Y LO ASIGNO COMO VALOR A LA CONSTANTE PRODUCTSCONTAINER:
 
-
-//* 6. RECORRO EL ARRAY DE PRODUCTOS Y CREO UNA CARD POR CADA UNO DE ELLOS:
-
-products.forEach((product) => {
-    // 6.1 CREO CONTENEDOR DIV DONDE SE AGREGARAN TODAS LAS CARDS:
-    const card = document.createElement('div');
-    // 6.2 lES AGREGO LA CLASE NECESARIA:
-    card.className = "card";
-    // 6.3 CON INNER.HMTL INYECTO EL HTML DE LA CARD:
-    card.innerHTML = `
-        <img src=${product.img} class="card-img-top rounded mx-auto d-block" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${product.name}</h5>
-            <p class="card-text">${product.description}</p>
-            <p class="card-text">${product.price} ARS</p>
-            <button class="button btn-primary" data-id="${product.id}">AÑADIR AL CARRITO</button>
-        </div>
-    `;
-    // 6.4 CON EL METODO .APPEND, AGREGO LAS CARDS AL CONTENEDOR:
-    productsContainer.append(card);
-});
+const productsContainer = document.querySelector('.products');
 
 
-//* 7. ACCEDO A LOS BOTONES DE "AGREGAR AL CARRITO" Y ESTO ME DEVOLVERA UN ARRAY CON TODOS LOS BOTONES CON LA CLASE "BUTTON" QUE SE HAYAN CREADO CON LA FUNCION:
+//* 4. CREO UNA FUNCION QUE RECORRA EL ARRAY DE PRODUCTOS Y CREE UNA CARD POR CADA UNO DE ELLOS:
+// (ESTA FUNCION SE LLAMARA LUEGO CUANDO TRAIGA LOS PRODUCTOS DEL JSON)
 
-const buyBtns = document.querySelectorAll('.button');
+const renderProducts = () => {
+    products.forEach((product) => {
+        // 6.1 CREO CONTENEDOR DIV DONDE SE AGREGARAN TODAS LAS CARDS:
+        const card = document.createElement('div');
+        // 6.2 lES AGREGO LA CLASE NECESARIA:
+        card.className = "products__card card";
+        // 6.3 CON INNER.HMTL INYECTO EL HTML DE LA CARD:
+        card.innerHTML = `
+            <img src=${product.img} class="card__img" alt="...">
+            <div class="card__body">
+                <h5 class="card__title">${product.name}</h5>
+                <p class="card__text">${product.description}</p>
+                <p class="card__price">${product.price} ARS</p>
+                <button class="card__button" data-id="${product.id}">AGREGAR AL CARRITO</button>
+            </div>
+        `;
+        // 6.4 CON EL METODO .APPEND, AGREGO LAS CARDS AL CONTENEDOR:
+        productsContainer.append(card);
+    });
+
+}
+
+//* 5. CREO UNA FUNCION PARA LLAMAR A LOS PRODUCTOS DESDE JSON Y LA EJECUTO:
+
+const loadProducts = async () => {
+    // showLoader();
+    const response = await fetch('../data/products.json')
+    const { data } = await response.json()
+    products = data
+    renderProducts(products);
+    // hideLoader();
+};
+
+loadProducts();
 
 
-//* 8. CREO UNA FUNCION PARA ELIMINAR UN PRODUCTO DEL CARRITO:
+//* 6. ACCEDO A LOS BOTONES DE "AGREGAR AL CARRITO" Y ESTO ME DEVOLVERA UN ARRAY CON TODOS LOS BOTONES CON LA CLASE "CARD__BUTTON" QUE SE HAYAN CREADO CON LA FUNCION RENDERPRODUCTS:
+
+const buyBtns = document.querySelectorAll('.card__button');
+
+
+//* 7. CREO UNA FUNCION PARA ELIMINAR UN PRODUCTO DEL CARRITO:
 
 const deleteProductFromCart = () => {
     // A. ACCEDO A LOS BOTONES DE "ELIMINAR PRODUCTO" Y ESTO ME DEVOLVERA UN ARRAY CON TODOS LOS BOTONES CON LA CLASE "DELETE-BTN" QUE SE HAYAN CREADO CON LA FUNCION PRINTCART.
-    const deleteProductBtns = document.querySelectorAll('.delete-btn');
+    const deleteProductBtns = document.querySelectorAll('.cart__delbtn');
     // B. RECORRO EL ARRAY DE BOTONES CON UN .FOREACH Y LE AÑADO UN .ADDEVENTLISTENER() A CADA UNO DE LOS BOTONES:
     deleteProductBtns.forEach((button) => {
         button.addEventListener('click', (e) => {
@@ -82,10 +78,10 @@ const deleteProductFromCart = () => {
 };
 
 
-//* 9. CREO UNA FUNCION "IMPRIMIR CARRITO" PARA UTILIZAR EN LA SIGUIENTE FUNCION COMO EVENTO DE CLICK "AGREGAR PRODUCTO AL CARRITO":
+//* 8. CREO UNA FUNCION "IMPRIMIR CARRITO" PARA UTILIZAR EN LA SIGUIENTE FUNCION COMO EVENTO DE CLICK "AGREGAR PRODUCTO AL CARRITO":
 
-// 9.1 SELECCIONO EL CONTENEDOR DEL CARRITO Y LO ASIGNO COMO VALOR A LA VARIABLE CARTCONTAINER: 
-const cartContainer = document.querySelector('.cart-container');
+// 8.1 SELECCIONO EL CONTENEDOR DEL CARRITO Y LO ASIGNO COMO VALOR A LA VARIABLE CARTCONTAINER: 
+const cartContainer = document.querySelector('.cart');
 
 const printCart = () => {
     // A. PRIMERO INYECTO CODIGO HTML VACIO:
@@ -95,14 +91,14 @@ const printCart = () => {
         // B.1 CREO UNA CONSTANTE Y LE ASIGNO COMO VALOR LA CREACION DE UN CONTENEDOR DIV QUE SERA EL CONTENEDOR DE LOS PRODUCTOS DEL CARRITO:
         const cartView = document.createElement('div');
         // B.2 LE ASIGNO LA CLASE NECESARIA:
-        cartView.className = "card";
+        cartView.className = "cart__container";
         // B.3 LE ASIGNO EL HTML DE CADA PRODUCTO DEL CARRITO:
         cartView.innerHTML = `
-            <img src=${product.img} class="card-img-top rounded mx-auto d-block" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${product.name}</h5>
-                <p class="card-text">${product.price} ARS</p>
-                <button class="button btn-danger delete-btn" data-id="${product.id}">ELIMINAR DEL CARRITO</button>
+            <img src=${product.img} class="cart__img" alt="...">
+            <div class="cart__body">
+                <h5 class="cart__title">${product.name}</h5>
+                <p class="cart__text">${product.price} ARS</p>
+                <button class="cart__delbtn" data-id="${product.id}">ELIMINAR DEL CARRITO</button>
             </div>
         `;
         // B.4 LE AÑADO EL CONTENEDOR DE EL/LOS PRODUCTOS DEL CARRITO AL CONTENEDOR DEL CARRITO GENERAL:
@@ -122,11 +118,11 @@ const addProductToCart = (e) => {
     const product = products.find(product => product.id === selectProduct);
     // C. REALIZO UN .PUSH() DEL PRODUCTO ELEGIDO AL CARRITO:
     cart.push(product);
-    // E. IMPRIMIMOS EL CARRITO EN LA PANTALLA:
+    // D. IMPRIMIMOS EL CARRITO EN LA PANTALLA:
     printCart();
-    // G. IMPRIMIMOS EL CARRITO EN EL LOCALSTORAGE:
+    // E. IMPRIMIMOS EL CARRITO EN EL LOCALSTORAGE:
     localStorage.setItem('cart', JSON.stringify(cart));
-
+    // F. INFORMAMOS AL USUARIO QUE EL PRODUCTO SE HA AGREGADO AL CARRITO:
     Toastify({
         text: "Agregado al carrito",
         duration: 2000,
@@ -146,14 +142,14 @@ const addProductToCart = (e) => {
 };
 
 
-//* 10.1 HABIENDO CREADO EL ARRAY DE BOTONES Y LA FUNCION DE AGREGAR PRODUCTOS AL CARRITO, AHORA RECORRO EL ARRAY DE PRODUCTOS CON UN .FOREACH Y LE AGREGO UN .ADDEVENTLISTENER() A CADA UNO DE LOS BOTONES, DE ESA MANERA LO PODRA ESCUCHAR CUANDO SE CLICKEE Y EJECUTARA LA FUNCION YA DECLARADA:
+//* 11. HABIENDO CREADO EL ARRAY DE BOTONES Y LA FUNCION DE AGREGAR PRODUCTOS AL CARRITO, AHORA RECORRO EL ARRAY DE PRODUCTOS CON UN .FOREACH Y LE AGREGO UN .ADDEVENTLISTENER() A CADA UNO DE LOS BOTONES, DE ESA MANERA LO PODRA ESCUCHAR CUANDO SE CLICKEE Y EJECUTARA LA FUNCION YA DECLARADA:
 
 buyBtns.forEach((buyButton) => {
     buyButton.addEventListener('click', addProductToCart);
 });
 
 
-//* 5. VERIFICO SI EL LOCALSTORAGE TIENE ALGO Y SI ES ASI, LO CARGO EN EL ARRAY DE PRODUCTOS DEL CARRITO:
+//* 12. VERIFICO SI EL LOCALSTORAGE TIENE ALGO Y SI ES ASI, LO CARGO EN EL ARRAY DE PRODUCTOS DEL CARRITO:
 
 if (localStorage.getItem('cart')) {
     cart = JSON.parse(localStorage.getItem('cart'));
@@ -161,8 +157,14 @@ if (localStorage.getItem('cart')) {
 };
 
 
+//* 13. CUANDO HAGO CLICK EN .NAV__BTN, TOGGLE 'ACTIVE':
 
+const cartButton = document.querySelector('.nav__btn');
+const cartDiv = document.querySelector('.cart');
 
+cartButton.addEventListener('click', () => {
+    cartDiv.classList.toggle('active');
+});
 
 
 
